ab7008f5762fecb11b0fa70c69b8e8d10e2f3373
shutsuchi <wwww.www.wshu@gmail.com>
2020-02-04 19:24:30 +0900

    [InProgress] User-(register&login) Function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -12,23 +19,40 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def update
-    user = user.new(user_params)
-    user.update
-    redirect_to
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render edit_user_path(@user)
+    end
   end
 
   def destry
     user = user.find(params[:id])
     user.destroy
-    redirect_to
+    #redirect_to
   end
 
+  Protected
+    def configure_permitted_parameters
+    end
 
-  private user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
-  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :introduction, :profile_image, :password, :password_confirmation)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to rrot_url unless current_user?(@user)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get 'home/about'
 
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/login', to: 'session#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :edit, :update, :index]
 end
